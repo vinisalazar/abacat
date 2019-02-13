9914d5195d915344c875e33eb2e101094beba5f9
Vini Salazar <viniws@gmail.com>
2019-02-13 17:09:21 -0200

    Improving blast command.

--- gene_blaster.py
+++ gene_blaster.py
@@ -19,7 +19,7 @@ def call_blast(query, out_dir, outfmt, db='nucl', blast_type="n"):
     out_file = os.path.join(out_dir, os.path.basename(query.split('.')[0])+'_blast_out.tsv')
 
     if str.lower(blast_type) in ("p", "blastp", "protein"):
-        cmd = f"blastp -query {query} -db {db} -outfmt '{outfmt}' -out {out_file} -num_descriptions 1 -num_alignments 5 -remote"
+        cmd = f"blastp -query {query} -db {db} -outfmt '{outfmt}' -out {out_file} -max_target_seqs 20 -remote"
     else:
         cmd = f"blastn -query {query} -db {db} -outfmt {outfmt} -out {out_file} -num_alignments 20"
 
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Will perform blast on sequence files inside the input directory.")
     parser.add_argument("-i", "--in_dir", help="Input directory with genes.")
     parser.add_argument("-o", "--out_dir", help="Output directory for blast outputs.")
-    parser.add_argument("-fmt", "--out_fmt", help="Blast format type.", default=1)
+    parser.add_argument("-fmt", "--outfmt", help="Blast format type.", default="7", type=str)
     parser.add_argument("-db", "--ncbi_db", help="NCBI database to search.")
     parser.add_argument("-b", "--blast_type", help="Blast types, 'n' for blastn and 'p' for blastp.", default="n")
 
@@ -45,7 +45,7 @@ if __name__ == "__main__":
 
     for query in files:
         try:
-            call_blast(query, args.out_dir, args.out_fmt, args.ncbi_db, args.blast_type)
+            call_blast(query, args.out_dir, args.outfmt, args.ncbi_db, args.blast_type)
         except:
             raise
             pass
