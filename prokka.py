6858267ff1bb297b6d722971c0e044c858367ecf
Vini Salazar <viniws@gmail.com>
2019-03-31 19:28:12 -0300

    Brand new Prokka script ⚡️♒️

--- prokka.py
+++ prokka.py
@@ -1,18 +1,113 @@
+"""
+A script to call Prokka to do it's things. Similar to prodigal.py.
+
+Input:
+file or folder with contigs in fasta or fna format.
+
+Output:
+Prokka folder to selected output (default current directory).
+
+Example usage:
+
+    python prokka.py -i contigs/
+    python prokka.py -i contigs.fasta
+
+"""
+
+import os
 import sys
+import time
+import argparse
+import datetime
 import subprocess
+from helper_functions import is_fasta, is_fasta_wrapper
 
-with open('fasta/prochlorococcus.txt') as f:
-    r = f.readlines()
-    r = [i.strip() for i in r]
 
-for i in r:
-    subprocess.call(
-        [
-            "prokka",
-            "--outdir",
-            f"prokka_runs/{i[:-6]}",
-            "--genus",
-            "Prochloroccus",
-            f"fasta/prochlorococcus/{i}"
-        ]
-    )
+@is_fasta_wrapper
+def prokka(file, output="", cpus=None):
+    """
+    Calls Prokka on an input file.
+
+    Input:
+    Valid FASTA file.
+
+    Output:
+    Prokka output to selected folder.
+
+    Example:
+
+        from prokka import prokka
+        prokka("contigs.fasta", "output_folder/")
+    """
+
+    if output:
+        output = os.path.join(os.path.abspath(output), os.path.splitext(file)[0])
+    else:
+        output = os.path.basename(os.path.splitext(file)[0])
+
+    output = f"--outdir {output}_prokka"
+
+    if not cpus:
+        cpus = int(os.cpu_count() / 2)
+
+    cpus = f"--cpus {cpus}"
+
+    cmd = f"prokka {output} {file} {cpus}"
+
+    prokka = subprocess.call(cmd, shell=True)
+
+
+if __name__ == "__main__":
+
+    start = time.time()
+
+    parser = argparse.ArgumentParser(description="""
+    A script to call Prodigal to predict both genes and proteins.
+    """)
+
+    parser.add_argument("-i", "--input", help="Input FASTA file or dir containing fasta files")
+    parser.add_argument("-o", "--output", help="Path to output folder", default="")
+
+    args = parser.parse_args()
+
+    if not args.input:
+        parser.print_help()
+        sys.exit(0)
+
+    input = args.input
+
+    if os.path.isfile(input):
+        print(f"Starting script. Your input file is {input[0]}.")
+        prokka(input[0], args.output)
+
+    elif os.path.isdir(input):
+
+        files = os.listdir(input)
+        files = [os.path.join(input, i) for i in files]
+        files = [i for i in files if os.path.isfile(i)]
+
+        print("\n")
+        print(f"Starting script. You have {len(files)} files to be processed in {input}:\n")
+        print("\n".join(files), "\n")
+
+        success = 0
+        failure = 0
+
+        for i in files:
+            try:
+                print(f"Running Prokka for {i}.")
+                prokka(i, args.output)
+                if os.path.isdir(os.path.splitext(i)[0]):
+                    success += 1
+            except Exception as err:
+                print(f"Error for {i}. Please check if it is a valid FASTA file.")
+                failure += 1
+                pass
+    else:
+        raise FileNotFoundError
+
+    end = time.time()
+    delta = str(datetime.timedelta(seconds=end - start))
+
+    print("\n")
+    print(f"Done. {success} assemblies processed. {failure} errors. Took {delta}.")
