89390b6731d3a5acb83e2f00ef18e59a5fdfec0e
Vini Salazar <viniws@gmail.com>
2019-04-02 09:41:31 -0300

    Added execution bash scripts

---
+++ assembly_prokka.sh
@@ -0,0 +1,18 @@
+echo "Starting AssemblyTools."
+
+echo "Removing old files."
+rm -rf ncbi-genomes-2018-12-20
+
+echo "Decompressing new files."
+tar zxf genome_assemblies.tar
+
+echo "Renaming files."
+python /Users/viniWS/Bio/masters/scripts/assembly_tools/renamer.py -b $1 
+
+echo "Moving genomic files to folder."
+rm -rf genomic_fna/
+mkdir genomic_fna/
+mv $1/*/*genomic.fna genomic_fna/
+
+echo "Starting Prodigal."
+python /Users/viniWS/Bio/masters/scripts/assembly_tools/prokka.py -i genomic_fna/
