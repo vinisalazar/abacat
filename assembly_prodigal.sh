a3f9e7e821e8256ea612bee26959477040a1b4d2
Vini Salazar <viniws@gmail.com>
2019-04-08 19:42:49 -0300

    Added complete paths v2

--- assembly_prodigal.sh
+++ assembly_prodigal.sh
@@ -4,15 +4,15 @@ echo "Removing old files."
 rm -rf /Users/viniWS/Bio/masters/test_data/ncbi/ncbi-genomes-2018-12-20
 
 echo "Decompressing new files."
-tar zxf /Users/viniWS/Bio/masters/test_data/ncbi/genome_assemblies.tar
+tar -C /Users/viniWS/Bio/masters/test_data/ncbi/ -zxf /Users/viniWS/Bio/masters/test_data/ncbi/genome_assemblies.tar
 
 echo "Renaming files."
-python /Users/viniWS/Bio/masters/scripts/assembly_tools/renamer.py -b $1
+python /Users/viniWS/Bio/masters/scripts/assembly_tools/renamer_v2.py -i /Users/viniWS/Bio/masters/test_data/ncbi/ncbi-genomes-2018-12-20
 
 echo "Moving genomic files to folder."
-rm -rf genomic_fna/
-mkdir genomic_fna/
-mv $1/*/*genomic.fna genomic_fna/
+rm -rf /Users/viniWS/Bio/masters/test_data/ncbi/genomic_fna/
+mkdir /Users/viniWS/Bio/masters/test_data/ncbi/ncbi-genomes-2018-12-20/genomic_fna/
+mv /Users/viniWS/Bio/masters/test_data/ncbi/ncbi-genomes-2018-12-20/*/*genomic.fna /Users/viniWS/Bio/masters/test_data/ncbi/ncbi-genomes-2018-12-20/genomic_fna/
 
-echo "Starting Prodigal."
-python /Users/viniWS/Bio/masters/scripts/assembly_tools/prodigal.py -i genomic_fna/
+#echo "Starting Prodigal."
+#python /Users/viniWS/Bio/masters/scripts/assembly_tools/prodigal.py -i /Users/viniWS/Bio/masters/test_data/ncbi/ncbi-genomes-2018-12-20/genomic_fna/
