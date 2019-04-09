183379f32c16e087f3c3347f7985c02113bcc1a6
Vini Salazar <viniws@gmail.com>
2019-04-09 10:07:45 -0300

    Added timer wrapper

--- helper_functions.py
+++ helper_functions.py
@@ -1,7 +1,8 @@
 """
 A file containing some helper functions.
 """
-
+import datetime
+import time
 from Bio import SeqIO
 
 
@@ -19,10 +20,29 @@ def is_fasta_wrapper(func):
     """
     A decorator wrapper for functions which need checking for a valid FASTA.
     """
+
     def wrapper(*args, **kwargs):
         if not is_fasta(args[0]):
             raise Exception(
                 "Your file is not valid. Please check if it is a valid FASTA file."
             )
         func(*args, **kwargs)
+
+    return wrapper
+
+
+def timer_wrapper(func):
+    """
+    A wrapper to time the execution time of our functions.
+    """
+
+    def wrapper(*args, **kwargs):
+        start = time.time()
+
+        func(*args, **kwargs)
+
+        end = time.time()
+        delta = str(datetime.timedelta(seconds=end - start))
+        print(f"Took {delta}")
+
     return wrapper
