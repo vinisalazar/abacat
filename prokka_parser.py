5b006d10bca85b1f1e6f8eb17adea86534b3c598
Vini Salazar <viniws@gmail.com>
2019-04-10 16:36:02 -0300

    Changed SSU finder to ffn parser.

--- prokka_parser.py
+++ prokka_parser.py
@@ -1,8 +1,6 @@
 """
 A script to parse Prokka outputs.
 
-Our first goal is to extract 16S sequences. (ffn file)
-
 Input:
 Directory output of Prokka.
 """
@@ -10,40 +8,52 @@ import os
 from Bio import SeqIO
 from helper_functions import is_fasta_wrapper, timer_wrapper
 
-file = "/Users/viniWS/Bio/masters/test_data/own_data/CCMR0080_newscaffold_prokka/PROKKA_04092019.ffn"
-
 
 @is_fasta_wrapper
-def ssu_finder(input_file, write=True):
+def ffn_parser(ffn_file, write=True):
     """
-    Scans Prokka output .ffn file for 16S sequences.
+    Scans Prokka output .ffn file and creates files with SSU seqs,
+    known proteins and hypothetical proteins.
     """
 
-    records = SeqIO.parse(input_file, "fasta")
-    ssu = []
+    records = SeqIO.parse(ffn_file, "fasta")
+    ssu, known_prots, hypothetical_prots = (
+        ("SSU", []),
+        ("known_prots", []),
+        ("hypothetical_prots", []),
+    )
+
+    # Generate our output file name.
+    output = os.path.splitext(os.path.abspath(ffn_file))[0]
+
     for seq in records:
         if "16S ribosomal RNA" in seq.description:
-            ssu.append(seq)
+            ssu[1].append(seq)
+        elif "hypothetical" in seq.description:
+            hypothetical_prots[1].append(seq)
+        else:
+            known_prots[1].append(seq)
 
-    if len(ssu) >= 1:
-        print(f"Found {len(ssu)} 16S sequences in {input_file}.")
-        output = os.path.splitext(os.path.abspath(input_file))[0] + "_ssu.fna"
-        if write:
-            print(f"Writing {len(ssu)} sequences to {output}.")
-            with open(output, "w") as f:
-                SeqIO.write(ssu, f, "fasta")
-        return ssu
-    else:
-        print("No SSU sequences found. Please check your file.")
+    for seq_type, sequences in (ssu, known_prots, hypothetical_prots):
+        if len(sequences) >= 1:
+            print(f"Found {len(sequences)} {seq_type} sequences in {ffn_file}.")
+            if write:
+                output_ = output + f"_{seq_type}.fna"
+                with open(output_, "w") as f:
+                    SeqIO.write(sequences, f, "fasta")
+                    print(f"Wrote {len(sequences)} {seq_type} sequences to {output_}.")
+
+    return ssu[1], known_prots[1], hypothetical_prots[1]
 
 
+# file = "/Users/viniWS/Bio/masters/test_data/own_data/CCMR0080_newscaffold_prokka/PROKKA_04092019.ffn"
 # @timer_wrapper
 # def main():
 #     files = [
 #         "/Users/viniWS/Bio/masters/test_data/own_data/CCMR0080_newscaffold_prokka/PROKKA_04092019.ffn",
 #         "/Users/viniWS/Bio/masters/test_data/own_data/CCMR0082_newscaffold_prokka/PROKKA_04092019.ffn",
-#         "/Users/viniWS/Bio/masters/test_data/own_data/CCMR0082_newscaffold_prokka/PROKKA_04092019.ffn",
-#         "/Users/viniWS/Bio/masters/test_data/own_data/CCMR0082_newscaffold_prokka/PROKKA_04092019.ffn",
+#         "/Users/viniWS/Bio/masters/test_data/own_data/CCMR0083_newscaffold_prokka/PROKKA_04092019.ffn",
+#         "/Users/viniWS/Bio/masters/test_data/own_data/CCMR0085_newscaffold_prokka/PROKKA_04092019.ffn",
 #     ]
 #     for file in files:
 #         ssu_finder(file)
