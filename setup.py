d3e984695a974e02b3ba0f4003b8272c3a867f29
Vini Salazar <viniws@gmail.com>
2019-04-10 23:37:56 -0300

    Experimenting with packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="AssemblyTools-Vini-Salazar",
+    version="0.0.1",
+    author="Vini Salazar",
+    author_email="viniws@gmail.com",
+    description="Python tools to work with microbial genome assemblies.",
+    packages=setuptools.find_packages(),
+)
