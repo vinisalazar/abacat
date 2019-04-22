df50c0abd947cbbdf4d18d2caf24bd0422e0b41d
Vini Salazar <viniws@gmail.com>
2019-04-22 14:35:46 -0300

    Starting OO project

---
+++ bactools.py
@@ -0,0 +1,13 @@
+"""
+A file for defining classes.
+"""
+
+
+class Assembly(object):
+    """
+    Assembly, a class containing bacterial assembly data and methods.
+    """
+
+    def __init__(self, arg):
+        super(Assembly, self).__init__()
+        self.arg = arg
