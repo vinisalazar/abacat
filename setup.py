13bc3c11a695c3e2531821fc1a070c618b821c39
Vini Salazar <viniws@gmail.com>
2019-05-02 10:42:40 -0300

    Change module name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="bactools:-Vini-Salazar",
+    name="bactools",
     version="0.0.1",
     author="Vini Salazar",
     author_email="viniws@gmail.com",
