37b8e42221e49d2fe99006101bfd1646d4e63f79
Vini Salazar <viniws@gmail.com>
2019-05-03 03:42:38 -0300

    Renamed bactools file, fixed imports on __init__

--- bactools/__init__.py
+++ bactools/__init__.py
@@ -1,6 +1,6 @@
 name = "bactools"
 
-from .bactools import *
-from .bactools_helper import *
-from .prodigal import *
-from .prokka import *
+from .assembly import Assembly, load_from_fasta
+from .bactools_helper import get_records, is_fasta, is_fasta_wrapper, timer_wrapper
+from .prodigal import prodigal
+from .prokka import prokka
