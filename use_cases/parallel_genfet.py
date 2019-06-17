f95705867779d9dbfa61016a52f5574d68104c80
Vini Salazar <viniws@gmail.com>
2019-06-17 12:36:49 -0300

    Added arguments and debugged parallel script

--- use_cases/parallel_genfet.py
+++ use_cases/parallel_genfet.py
@@ -7,24 +7,33 @@ import argparse
 import multiprocessing as mp
 from bactools import CONFIG, GenomeFetch, Query
 from Bio import Entrez
+from operator import methodcaller
 
 Entrez.email = CONFIG["email"]
 
-def main(file_with_accessions, fasta=False, gb=True, force=False):
+def main(file_with_accessions):
     """
     file_with_accessions: our input with all accession numbers we want to
     fetch from NCBI.
     """
     genfet = GenomeFetch(file_with_accessions)
 
-    with mp.Pool() as p:
-        p.map(Query.fetch_query, genfet.accessions)
+    return genfet.accessions
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input")
+    parser.add_argument("-nf", "--nofasta", dest="fasta", action="store_false")
+    parser.add_argument("-ng", "--nogb", dest="gb", action="store_false")
+    parser.add_argument("-f", "--force", dest="force", action="store_true")
+    parser.add_argument("-t", "--threads", default = int(os.cpu_count() / 2))
+    parser.set_defaults(fasta=True, gb=True, force=False)
 
     args = parser.parse_args()
 
-    main(args.input)
+    acc = main(args.input)
+
+    with mp.Pool(args.threads) as p:
+        f = methodcaller('fetch_query', force=args.force, fasta=args.fasta, gb=args.gb)
+        p.map(f, acc)
