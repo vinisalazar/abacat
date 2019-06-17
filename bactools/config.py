9ec2c61155e04156b53c5c5abf0b8e3853678eb1
Vini Salazar <viniws@gmail.com>
2019-06-17 16:03:14 -0300

    Fixed config

--- bactools/config.py
+++ bactools/config.py
@@ -1,8 +1,6 @@
-import os
-
-
 CONFIG = {
-    "db": "/Users/viniWS/storage/neorefs/rev6/neoRef16S_rev6_190325.acc",
-    "out_dir": "/Users/viniWS/storage/neorefs/genomes/bacteria",
-    "email": "vinicius.salazar@neoprospecta.com",
-}
+    "db" : "/home/ubuntu/storage/neorefs/neoref16s/rev6/neoRef16S_rev6_190325.acc",
+    "out_dir" : "/home/ubuntu/storage/neorefs/genomes/bacteria",
+    "email" : "vinicius.salazar@biome-hub.com",
+    "api_key": "afb857c557897af55345e77a84def712cd08"
+    }
