0694b6e5b15afeca0ae7b44a189c8ce4a32b8b0d
Vini Salazar <viniws@gmail.com>
2019-08-16 12:20:16 -0300

    Added path to megares db

--- bactools/config.py
+++ bactools/config.py
@@ -1,5 +1,6 @@
 CONFIG = {
     "db": {
-        "COG": "/Users/viniWS/Bio/db/COG/prot2003-2014.fa"
+        "COG": "/Users/viniWS/Bio/db/COG/prot2003-2014.fa",
+        "megares": "/Users/viniWS/Bio/db/megares_v1.01/megares_database_v1.01.fasta"
     }
 }
