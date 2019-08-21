e97268da5a8a2efad0feab572f3c57374fac140c
Vini Salazar <viniws@gmail.com>
2019-08-21 20:43:37 -0300

    Add default blast param

--- pipelines/annotate.py
+++ pipelines/annotate.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Annotation pipeline. Starts with a contig file.")
     parser.add_argument("-i", "--input", help="Input file. Must a valid FASTA contigs file (post-assembly).")
     parser.add_argument("-db", "--database", type=str, help="Database name. Must be in bactools.CONFIG.py db parameter.")
-    parser.add_argument("-b", "--blast", type=str, help="Blast method. Choose from 'blastn', 'blastp' or 'blastx'. Default is 'blastn")
+    parser.add_argument("-b", "--blast", type=str, default="n", help="Blast method. Choose from 'blastn', 'blastp' or 'blastx'. Default is 'blastn")
     args = parser.parse_args()
     main(args.input, args.database, args.blast)
 
