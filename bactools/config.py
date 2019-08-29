a80449b3261f6cde2e52b5f0239e511892bab4e7
Vini Salazar <viniws@gmail.com>
2019-08-28 21:51:43 -0300

    Added evalue as config parameter

--- bactools/config.py
+++ bactools/config.py
@@ -3,7 +3,8 @@ import os
 CONFIG = {
     "db": {
         "COG": "/Users/viniWS/Bio/db/COG/prot2003-2014.fa",
-        "megares": "/Users/viniWS/Bio/db/megares_v1.01/megares_database_v1.01.fasta"
+        "megares": "/Users/viniWS/Bio/db/megares_v1.01/megares_database_v1.01.fasta",
     },
-    "threads": int(os.cpu_count() / 2)
+    "threads": int(os.cpu_count() / 2),
+    "blast": {"evalue": 10 ** -20},
 }
