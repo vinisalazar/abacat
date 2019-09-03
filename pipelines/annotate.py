24d7bdce264221aec83c8a5179d22468bfd4ac2e
Vini Salazar <viniws@gmail.com>
2019-09-03 19:57:56 -0300

    Added evalue param to annotate.py

--- pipelines/annotate.py
+++ pipelines/annotate.py
@@ -11,10 +11,10 @@ Example annotation pipeline.
 
 """
 import argparse
-from bactools import Genome, timer_wrapper
+from bactools import Genome, timer_wrapper, CONFIG
 
 
-def annotate(input_, db, blast):
+def annotate(input_, db, blast, evalue):
     """
     :param input_: Input file. Must a valid FASTA contigs file (post-assembly).
     :param db: Database name. Must be in bactools.CONFIG.py db parameter.
@@ -25,19 +25,43 @@ def annotate(input_, db, blast):
     genome.load_seqstats()
     genome.print_seqstats()
     genome.run_prodigal()
-    genome.blastn_seqs(db=db, blast=blast)
+    genome.blastn_seqs(db=db, blast=blast, evalue=evalue)
     return genome
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Annotation pipeline. Starts with a contig file.")
-    parser.add_argument("-i", "--input", help="Input file. Must a valid FASTA contigs file (post-assembly).")
-    parser.add_argument("-db", "--database", type=str, help="Database name. Must be in bactools.CONFIG.py db parameter.")
-    parser.add_argument("-b", "--blast", type=str, default="n", help="Blast method. Choose from 'blastn', 'blastp' or 'blastx'. Default is 'blastn'")
+    parser = argparse.ArgumentParser(
+        description="Annotation pipeline. Starts with a contig file."
+    )
+    parser.add_argument(
+        "-i",
+        "--input",
+        help="Input file. Must a valid FASTA contigs file (post-assembly).",
+    )
+    parser.add_argument(
+        "-db",
+        "--database",
+        type=str,
+        help="Database name. Must be in bactools.CONFIG.py db parameter.",
+    )
+    parser.add_argument(
+        "-b",
+        "--blast",
+        type=str,
+        default="n",
+        help="Blast method. Choose from 'blastn', 'blastp' or 'blastx'. Default is 'blastn'",
+    )
+    parser.add_argument(
+        "-e",
+        "--evalue",
+        type=float,
+        default=CONFIG["blast"]["evalue"],
+        help="E-value for BLAST. Default is the one set in bactools/config.py",
+    )
     args = parser.parse_args()
 
     @timer_wrapper
     def run():
-        annotate(args.input, args.database, args.blast)
+        annotate(args.input, args.database, args.blast, args.evalue)
 
     run()
