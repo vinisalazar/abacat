5951a4223adcf1a8bd22f14d6406df698b576658
Vini Salazar <viniws@gmail.com>
2019-09-04 14:12:04 -0300

    Adding variable home path

--- abacat/config.py
+++ abacat/config.py
@@ -1,16 +1,17 @@
 import os
 import json
+from pathlib import Path
 
-CONFIG = {
-    "db": {
-        "COG": "/Users/viniWS/Bio/db/COG/prot2003-2014.fa",
-        "megares": "/Users/viniWS/Bio/db/megares_v1.01/megares_database_v1.01.fasta",
-        "phenotyping": "/Users/viniWS/Bio/db/phenotyping/phenotyping.fasta",
-        "pathways": "/Users/viniWS/Bio/abacat/pipelines/pathways.json"
-    },
-    "threads": int(os.cpu_count() / 2),
-    "blast": {"evalue": 10 ** -20},
-}
+def db(dir_name, file_name):
+    """
+    :param dir_name: Directory name inside the $db_path variable
+    :param file_name: File name inside $dir_name. Usually a fasta file.
+    :return: Joined path of db_path, dir_name, file_name
+    """
+    db = os.path.join(db_path, dir_name, file_name)
+    if not os.path.isfile(db):
+        raise Exception(f"Database not found at {db}. Please check if it is a valid path.")
+    return db
 
 
 def load_pathways(pathways_json):
@@ -20,4 +21,15 @@ def load_pathways(pathways_json):
     return pathways
 
 
+db_path = os.path.join(Path.home(), "Bio/db")
+CONFIG = {
+    "db": {
+        "COG": db("COG", "prot2003-2014.fa"),
+        "megares": db("megares_v1.01", "megares_database_v1.01.fasta"),
+        "phenotyping": db("phenotyping", "phenotyping.fasta"),
+        "pathways": "pipelines/pathways.json"
+    },
+    "threads": int(os.cpu_count() / 2),
+    "blast": {"evalue": 10 ** -20},
+}
 pathways = load_pathways(CONFIG["db"]["pathways"])
