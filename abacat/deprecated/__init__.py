59562fdcbd7fb30487d70cfe646394d71adff0a9
Vini Salazar <viniws@gmail.com>
2019-09-04 14:40:32 -0300

    Created __init__ for deprecated package

---
+++ abacat/deprecated/__init__.py
@@ -0,0 +1,4 @@
+from .prokka import prokka
+from .prokka_parser import ffn_parser
+from .renamer import ls_and_decompress, parse_assembly_report, rename_assembly
+from .reports import dict_from_report
