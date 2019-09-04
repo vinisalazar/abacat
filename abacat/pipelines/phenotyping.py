68ec74b99b0d1ac8c080504ec64a2bb9add48e7e
Vini Salazar <viniws@gmail.com>
2019-09-04 16:52:33 -0300

    Simplified script and added command line args

--- abacat/pipelines/phenotyping.py
+++ abacat/pipelines/phenotyping.py
@@ -1,50 +1,31 @@
-# import argparse
-from abacat import CONFIG, pathways, pipelines
-
 """
-This module uses the annotate function to predict metabolic pathways using the
-pathway.json file.
+Find phenotyping genes in a genome. Can be used with either a JSON or contigs input.
 """
 
-
-def phenotype(input_, blast="blastx", evalue=10 ** -3):
-    """
-    Parse an annotation to get phenotype.
-    :param input_: Contigs file
-    :param blast: Type of blast. Default is blastx
-    :return:
-    """
-    genome = pipelines.annotate(input_, db="phenotyping", blast=blast, evalue=evalue)
-
-    def pathway_genes(info=True):
-        """
-        Takes the phenotyping geneset records and checks them against the pathways object
-        from the CONFIG module.
-        :return: pathway genes, a dict containing pathways as keys and identified records as values.
-        """
-        pathway_genes = {}
-        for k, v in pathways.items():
-            pathway_genes[k] = []
-            for gene in genome.geneset["phenotyping"]["records"]:
-                desc = gene.description.split()[1].split(".")[1]
-                if desc in v:
-                    pathway_genes[k].append(gene)
-            if info:
-                print(f"Found {len(pathway_genes[k])} genes for {k};")
-
-        return pathway_genes
-
-    genome.geneset["phenotyping"]["pathways"] = pathway_genes()
-    return genome
+import argparse
+from abacat import Genome, from_json, timer_wrapper
 
 
-genome = phenotype(
-    "/Users/viniWS/Bio/abacat/data/synecho/GCA_900473895.1_N32/GCA_900473895.1_N32_genomic.fna"
-)
-target = "/Users/viniWS/Bio/abacat/data/synecho/"
+def main(input_, evalue, json=False):
+    if json:
+        g = from_json(input_)
+    else:
+        g = Genome(input_)
+        g.run_prodigal()
+
+    g.run_pathways(evalue=evalue)
+    g.to_json()
 
 
-from Bio.Blast import NCBIXML
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Get pathway genes from a genome. Will write json output file.")
+    parser.add_argument("-i", "--input", help="Input genome. Must be either: a valid contigs file or a json genome file.")
+    parser.add_argument("-e", "--evalue", type=float, default=10 ** -3, help="E-value for BLAST to Pathways DB")
+    parser.add_argument("-j", "--json", type=bool, default=False, help="Specifies that you're using an already processed JSON input.")
+    args = parser.parse_args()
 
-with open(genome.files["phenotyping"]["xml"]) as f:
-    p = list(NCBIXML.parse(f))
+    @timer_wrapper
+    def run():
+        main(args.input, args.evalue, args.json)
+
+    run()
