0bf0903570fabb9bcafde237c43e261883443f59
Vini Salazar <viniws@gmail.com>
2019-09-04 14:41:18 -0300

    Added include_package_data to setup config

--- setup.py
+++ setup.py
@@ -7,4 +7,5 @@ setuptools.setup(
     author_email="viniws@gmail.com",
     description="abacat - a bacterial annotation and curation toolkit",
     packages=setuptools.find_packages(),
+    include_package_data=True,
 )
