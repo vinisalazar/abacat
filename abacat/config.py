19b8d38880c9a887d38930b20c350d3f14ce788a
Vini Salazar <viniws@gmail.com>
2019-09-27 02:10:11 -0300

    Added local genomes and local db to config, fixed tests to match

--- abacat/config.py
+++ abacat/config.py
@@ -1,22 +1,19 @@
 import os
 import json
 from pathlib import Path
-from abacat.data import data_dir
+from abacat.data import data_dir, genomes_dir, local_db_dir
 
 abacat_path = os.path.dirname(__file__)
-
 external_db_path = os.path.join(Path.home(), "Bio/db")
-local_db_path = os.path.join(abacat_path, "data/db/")
 
-
-def db(dir_name, file_name, local=False):
+def db(dir_name, file_name, external=False):
     """
     :param dir_name: Directory name inside the $db_path variable
     :param file_name: File name inside $dir_name. Usually a fasta file.
     :return: Joined path of db_path, dir_name, file_name
     """
-    if local:
-        db = os.path.join(local_db_path, dir_name, file_name)
+    if not external:
+        db = os.path.join(local_db_dir, dir_name, file_name)
     else:
         db = os.path.join(external_db_path, dir_name, file_name)
     # if not os.path.isfile(db):
@@ -33,16 +30,22 @@ def load_phenotyping(phenotyping_json):
 
 CONFIG = {
     "db": {
-        "COG": db("COG", "prot2003-2014.fa", local=False),
-        "megares": db("megares_v1.01", "megares_database_v1.01.fasta", local=True),
-        "phenotyping": db("phenotyping", "phenotyping.fasta", local=True),
-        "phenotyping": os.path.join(data_dir, "phenotyping.json"),
+        "COG": db("COG", "prot2003-2014.fa", external=True),
+        "megares": db("megares", "megares_database_v1.01.fasta"),
+        "phenotyping": db("phenotyping", "phenotyping.fasta"),
+        "pathways": db("phenotyping", "pathways.json"),
     },
     "third_party": {"fastANI": "/Users/viniWS/Bio/FastANI/fastANI"},
     "threads": int(os.cpu_count() / 2),
     "blast": {"evalue": 10 ** -20},
-    "test_contigs": os.path.join(data_dir, "GCF_001021895.1_ASM102189v1_genomic.fna"),
+    "test_genomes": {
+        "Staphylococcus aureus CA15": os.path.join(genomes_dir, "GCF_001021895.1_ASM102189v1_genomic.fna"),
+        "Staphylococcus epidermidis ATCC 12228": os.path.join(genomes_dir, "GCF_000007645.1_ASM764v1_genomic.fna"),
+        "Synechococcus elongatus PCC 6301": os.path.join(genomes_dir, "GCF_000010065.1_ASM1006v1_genomic.fna"),
+        "Prochloroccocus marinus MIT 9313": os.path.join(genomes_dir, "GCF_000011485.1_ASM1148v1_genomic.fna"),
+        "Escherichia coli ATCC 1175": os.path.join(genomes_dir, "GCF_003697165.2_ASM369716v2_genomic.fna"),
+    },
     "data_dir": data_dir,
 }
 
-phenotyping = load_phenotyping(CONFIG["db"]["phenotyping"])
+pathways = load_phenotyping(CONFIG["db"]["pathways"])
