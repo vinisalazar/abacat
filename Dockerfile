766e0c60f59dc01ea63a997c13a7688fefc3791d
Vini Salazar <viniws@gmail.com>
2019-09-27 02:06:45 -0300

    Renaming files

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM continuumio/miniconda3
 RUN apt-get update -y; apt-get upgrade -y
 RUN conda update -n base -c defaults conda -y
-RUN conda install pandas pytest biopython matplotlib prodigal fastani scipy blast==2.9 -c conda-forge -c bioconda -y
+RUN conda install ipython pandas pytest biopython matplotlib prodigal fastani scipy blast==2.9 -c conda-forge -c bioconda -y
 RUN git clone https://github.com/vinisalazar/abacat.git
 WORKDIR /abacat
+
 RUN cd abacat/
 RUN pip install .
 RUN pytest .
--- abacat/config.py
+++ abacat/config.py
@@ -4,34 +4,39 @@ from pathlib import Path
 from abacat.data import data_dir
 
 abacat_path = os.path.dirname(__file__)
-db_path = os.path.join(Path.home(), "Bio/db")
+
+external_db_path = os.path.join(Path.home(), "Bio/db")
+local_db_path = os.path.join(abacat_path, "data/db/")
 
 
-def db(dir_name, file_name):
+def db(dir_name, file_name, local=False):
     """
     :param dir_name: Directory name inside the $db_path variable
     :param file_name: File name inside $dir_name. Usually a fasta file.
     :return: Joined path of db_path, dir_name, file_name
     """
-    db = os.path.join(db_path, dir_name, file_name)
+    if local:
+        db = os.path.join(local_db_path, dir_name, file_name)
+    else:
+        db = os.path.join(external_db_path, dir_name, file_name)
     # if not os.path.isfile(db):
     #    raise Exception(f"Database not found at {db}. Please check if it is a valid path.")
     return db
 
 
-def load_pathways(pathways_json):
-    with open(pathways_json) as f:
-        pathways = json.load(f)
+def load_phenotyping(phenotyping_json):
+    with open(phenotyping_json) as f:
+        phenotyping = json.load(f)
 
-    return pathways
+    return phenotyping
 
 
 CONFIG = {
     "db": {
-        "COG": db("COG", "prot2003-2014.fa"),
-        "megares": db("megares_v1.01", "megares_database_v1.01.fasta"),
-        "phenotyping": db("phenotyping", "phenotyping.fasta"),
-        "pathways": os.path.join(data_dir, "pathways.json"),
+        "COG": db("COG", "prot2003-2014.fa", local=False),
+        "megares": db("megares_v1.01", "megares_database_v1.01.fasta", local=True),
+        "phenotyping": db("phenotyping", "phenotyping.fasta", local=True),
+        "phenotyping": os.path.join(data_dir, "phenotyping.json"),
     },
     "third_party": {"fastANI": "/Users/viniWS/Bio/FastANI/fastANI"},
     "threads": int(os.cpu_count() / 2),
@@ -40,4 +45,4 @@ CONFIG = {
     "data_dir": data_dir,
 }
 
-pathways = load_pathways(CONFIG["db"]["pathways"])
+phenotyping = load_phenotyping(CONFIG["db"]["phenotyping"])
