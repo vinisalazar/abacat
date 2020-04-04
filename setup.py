8ffab75f834637aa2b64bc9411cd105679c650b1
Vini Salazar <viniws@gmail.com>
2020-04-04 18:27:21 -0300

    Add scripts and format with Black

--- setup.py
+++ setup.py
@@ -6,16 +6,22 @@ setuptools.setup(
     author="Vini Salazar",
     author_email="viniws@gmail.com",
     description="abacat - A BActerial genome Curation and Annotation Toolkit",
-    long_description="Abacat (pronounced "ABBA-cat") is a toolkit for working with bacterial whole genome sequencing (WGS) data. It provides Python objects to represent elements which are common to WGS analysis workflows, such as coding sequence (CDS) files, containing genes or proteins, alignment methods, or statistics about your sequences.",
+    long_description="Abacat (pronounced 'ABBA-cat') is a toolkit for working with bacterial whole genome sequencing (WGS) data. It provides Python objects to represent elements which are common to WGS analysis workflows, such as coding sequence (CDS) files, containing genes or proteins, alignment methods, or statistics about your sequences.",
     url="https://github.com/vinisalazar/abacat",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.6",
     ],
     packages=setuptools.find_packages(),
+    scripts=[
+        "abacat/prodigal.py",
+        "pipelines/annotate.py",
+        "pipelines/phenotyping.py",
+        "deprecated/prokka.py",
+    ],
     include_package_data=True,
     python_requires=">=3.6",
     install_requires=[
@@ -24,6 +30,6 @@ setuptools.setup(
         "argparse",
         "matplotlib",
         "scipy",
-        "pytest"
-    ]
+        "pytest",
+    ],
 )
